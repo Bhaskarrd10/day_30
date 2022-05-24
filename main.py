5e76ed71db6a0dd7b12bc4bdb9fe1146b23d2fc3
bhaskarreddy <bhaskrrd10@gmail.com>
2022-05-24 09:47:45 +0530

    raise a error

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-try:
-    file = open("a_file.text")
-    a_dictionary = {"key": "value"}
-    print(a_dictionary["key"])
-except FileNotFoundError:
-    file = open("a_file", "w")
-    file.write("something")
-except KeyError as error_message:
-    print(f"The key{error_message} does not exist")
-else:
-    content = file.read()
-    print(content)
-finally:
-    file.close()
-    print("file was closed")
+#try:
+#    file = open("a_file.text")
+#    a_dictionary = {"key": "value"}
+#    print(a_dictionary["key"])
+#except FileNotFoundError:
+#    file = open("a_file", "w")
+#    file.write("something")
+#except KeyError as error_message:
+#    print(f"The key{error_message} does not exist")
+#else:
+#    content = file.read()
+#    print(content)
+#finally:
+#    file.close()
+#    print("file was closed")
+
+
+height = float(input("Height: "))
+weight = int(input("weight: "))
+
+if height > 3:
+    raise ValueError("Human height should not be over 3 metere")
+
+bmi = weight/ height ** 2
+print(bmi)
